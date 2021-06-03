b860ff7d1a99bcadb8895bdc904410546af9cbb1
Tristan DAVID <tristandavid50@yahoo.fr>
2021-06-03 15:55:48 +0200

    Responsive

--- produits.js
+++ produits.js
@@ -1,9 +1,15 @@
-// Récupération URL pour récupérer ID du produit
-let currentUrl = window.location.href;
+async function fillProducts() {
+  await fetch('http://localhost:3000/api/teddies')
+    .then((response) => response.json())
+    .then((nounours) => {
 
-// ID du produit -> Split divise une chaîne de caract en liste de sous chaîne puis la place dans un tableau et retourne le tableau.
-const urlId = currentUrl.split('_id=');
-const productsId = urlId[1];
+      // Récupération URL pour récupérer ID du produit
+      let currentUrl = window.location.href;
 
-const api = "http://localhost:3000/api/teddies/";
-const producstUrl = api + urlId[1];
+      // ID du produit -> Split divise une chaîne de caract en liste de sous chaîne puis la place dans un tableau et retourne le tableau.
+      const urlId = currentUrl.split('_id=');
+      console.log(urlId);
+
+    })
+    .catch(error => console.log(error))
+}
